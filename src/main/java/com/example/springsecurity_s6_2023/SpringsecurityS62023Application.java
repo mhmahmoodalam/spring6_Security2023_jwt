311b04c5225f99f4bdd1105ac465e5c0bedc65dd
Muhammed Mahmood Alam <muhammed.alam.ext@sivantos.com>
2023-06-03 23:59:14 +0530

    database  connection configuration added google format updated gradle to v8

---
+++ src/main/java/com/example/springsecurity_s6_2023/SpringsecurityS62023Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity_s6_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityS62023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityS62023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurity_s6_2023/SpringsecurityS62023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity_s6_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityS62023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
